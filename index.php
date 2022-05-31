c6466fbb14c52638d0b5661ca4274bb6f6258382
Rinaldito <rinalditoryanari@gmail.com>
2022-05-31 07:42:26 +0700

    test

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
   <meta content="" name="description" />
   <meta content="" name="keywords" />
 
-  <!-- Favicons -->
+  <!-- test Favicons -->
   <link href="assets/img/favicon.png" rel="icon" />
   <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon" />
 
